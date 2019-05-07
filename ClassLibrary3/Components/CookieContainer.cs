20509f2a0b8e5fda4fbc301b57d855e15c8c00b3
vad7043 <vad7043@gmail.com>
2019-05-08 00:17:53 +0400

    фвцв

---
+++ ClassLibrary3/Components/CookieContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3.Components
+{
+    public class CookieWebClient : WebClient
+    {
+        public CookieContainer CookieContainer { get; private set; }
+        public CookieWebClient()
+        {
+            CookieContainer = new CookieContainer();
+        }
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address) as HttpWebRequest;
+            if (request == null) return base.GetWebRequest(address);
+            request.CookieContainer = CookieContainer;
+            return request;
+        }
+    }
+}
